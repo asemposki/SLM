c910de911622c7317623b834c2a20dff69314bee
Alexandra Semposki <as727414@ohio.edu>
2024-11-14 17:14:26 -0500

    adding testing suite folder

---
+++ tests/test_TOV_class.py
@@ -0,0 +1,14 @@
+###########################################
+# Unit tests of TOV_Class.py module
+# Author: Alexandra C. Semposki
+# Last edited: 14 November 2024
+###########################################
+
+import os
+import numpy as np
+import time
+
+from scipy.interpolate import interp1d
+
+# want testing functions that cover for a 
+# specific EOS we trust, e.g., SLy4.
