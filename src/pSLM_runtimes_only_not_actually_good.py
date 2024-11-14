134f2c882e7a238d561babfb6b7033b18f99c931
Joshua Maldonado <joshuamaldonado4432@gmail.com>
2024-11-14 18:19:01 -0500

    plots

--- src/pSLM_runtimes_only_not_actually_good.py
+++ src/pSLM_runtimes_only_not_actually_good.py
@@ -390,27 +390,17 @@ def main(tidal=False, mseos=False):
     for file in os.listdir(tov_data_path):
         fileList.append(file)
 
-    # random.seed(48824)
-    # fileList = sorted(fileList)
-    # updatedFileList = [file for file in fileList[::3]]
-    # print(len(updatedFileList))
-    # updatedFileList = updatedFileList[::4]
-    # # updatedFileList = random.sample(fileList, 20)
-    # testFileList = [file for file in fileList[::5] if file not in updatedFileList]
-    # print(len(testFileList))
-    
-    # testFileList = testFileList[::4]
-    # testFileList = random.sample(fileList, 10)  # originally 10
-    # testFileList = sorted(testFileList)
     random.seed(48824)
     fileList = sorted(fileList)
     updatedFileList = [file for file in fileList[::3]]
     print(len(updatedFileList))
     updatedFileList = updatedFileList[::4]
     # updatedFileList = random.sample(fileList, 20)
-    testFileList = [file for file in fileList[::4] if file not in updatedFileList]
+    testFileList = [file for file in fileList[::5] if file not in updatedFileList]
     print(len(testFileList))
-    testFileList = testFileList[::4]
+    # testFileList = testFileList[::4]
+    # testFileList = random.sample(fileList, 10)  # originally 10
+    # testFileList = sorted(testFileList)
 
     if not os.path.exists(TEST_DATA_PATH):
         os.makedirs(TEST_DATA_PATH)
@@ -437,8 +427,8 @@ def main(tidal=False, mseos=False):
             data = np.loadtxt(os.path.join(TEST_DATA_PATH, file))
             X = data.T
             name = "Data_" + "_".join(testParam)
-            # plot_parametric(Xdmd, X, name, tidal)
-            # print(f"plotted file: {name}")
+            plot_parametric(Xdmd, X, name, tidal)
+            print(f"plotted file: {name}")
             total_time = stoptime - starttime
             time_dict[file] = total_time
 
