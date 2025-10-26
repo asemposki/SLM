efabf9a79e762267522dbd2fdaa06532d7fa26ed
Sudhanva Lalit <sudsid@gmail.com>
2025-10-26 10:37:11 -0400

    Updated codes, included a test code

--- src/slmemulator/__init__.py
+++ src/slmemulator/__init__.py
@@ -1,22 +1,22 @@
+# __init__.py for automatic discovery
 import os
 import sys
-# from .recombination import *
-# from .SLM import *
-# from .TOV_class import TOV
-# from .plotData import *
-# from .config import get_paths
-# from .cleanData import *
-# from .pSLM import *
-# from .EOS_Codes import Quarkyonia, MSEOS
+
+# Import everything from the listed sub-modules:
+from .recombination import * # This will expose every function/class from recombination.py
+from .SLM import *
+from .TOV_class import *
+from .plotData import *
+from .config import * # This will expose get_paths (and anything else)
+from .cleanData import * # This will expose clean_directory (and anything else)
+from .pSLM import *
+from .EOS_Codes import * # This will expose MSEOS (and anything else)
+
+# This line is not needed if you use from .EOS_Codes import *
 # from .EOS_Codes.Quarkyonia import generate_quarkyonia_eos
 
-# from . import *
-# Add base directory (project root) to sys path
-# If __file__ is /project_root/src/slmemulator/__init__.py,
-# then this correctly calculates /project_root/
+# [Rest of your config and path code...]
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
-
-# Versioning (good practice)
 __version__ = "0.1.0"
